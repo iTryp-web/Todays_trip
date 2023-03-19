62b13fc8430505295a5d0a00736c0d3d8fcdfeff
jungyunmok <jungyunmok@gmail.com>
2023-03-19 17:54:53 +0900

    gitignore 추가

--- backend/src/main/java/com/backend/itryp/BackendApplication.java
+++ backend/src/main/java/com/backend/itryp/BackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  
 @SpringBootApplication
 public class BackendApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
