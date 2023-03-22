27c18e3bd85772abb3ac84fd4dcd2f2e5dbeba7a
jungyunmok <jungyunmok@gmail.com>
2023-03-22 21:52:18 +0900

    test

--- backend/src/main/java/com/backend/itryp/BackendApplication.java
+++ backend/src/main/java/com/backend/itryp/BackendApplication.java
@@ -3,9 +3,11 @@ package com.backend.itryp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  
+@Configuration
 @SpringBootApplication
 public class BackendApplication {
 	
