5bb15826b8014530b0e8eeb8d205055d18b14848
jungyunmok <jungyunmok@gmail.com>
2023-03-28 22:14:29 +0900

    03/28 BoardController 추가

--- backend/src/main/java/com/backend/itryp/dao/BoardDao.java
+++ backend/src/main/java/com/backend/itryp/dao/BoardDao.java
@@ -1,5 +1,109 @@
 package com.backend.itryp.dao;
 
-public class BoardDao {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardDao {
+	Logger logger = LogManager.getLogger(BoardDao.class);
+	
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate = null;
+
+	public List<Map<String, Object>> boardList(Map<String, Object> pMap) {
+		logger.info("boardList 호출");
+		List<Map<String,Object>> bList = null;
+		bList = sqlSessionTemplate.selectList("boardList", pMap);
+		return bList;
+	}
+
+	public List<Map<String, Object>> boardDetail(Map<String, Object> pMap) {
+		logger.info("boardDetail 호출");
+		List<Map<String,Object>> bList = null;
+		// bList = sqlSessionTemplate.selectList("boardDetail", pMap);
+		return bList;
+	}
+
+	public List<Map<String, Object>> boardHot(Map<String, Object> pMap) {
+		logger.info("boardHot 호출");
+		List<Map<String,Object>> bList = null;
+		// bList = sqlSessionTemplate.selectList("boardHot", pMap);
+		return bList;
+	}
+
+	public int boardInsert(Map<String, Object> pMap) {
+		logger.info("boardInsert 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("boardInsert", pMap);
+		return result;
+	}
+
+	public int boardUpdate(Map<String, Object> pMap) {
+		logger.info("boardUpdate 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("boardUpdate", pMap);
+		return result;
+	}
+
+	public int boardDelete(Map<String, Object> pMap) {
+		logger.info("boardDelete 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("boardDelete", pMap);
+		return result;
+	}
+
+	public List<Map<String, Object>> replyList(Map<String, Object> pMap) {
+		logger.info("replyList 호출");
+		List<Map<String,Object>> bList = null;
+		// bList = sqlSessionTemplate.selectList("replyList", pMap);
+		return bList;
+	}
+
+	public int replyInsert(Map<String, Object> pMap) {
+		logger.info("replyInsert 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("replyInsert", pMap);
+		return result;
+	}
+
+	public int replyUpdate(Map<String, Object> pMap) {
+		logger.info("replyUpdate 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("replyUpdate", pMap);
+		return result;
+	}
+
+	public int replyDelete(Map<String, Object> pMap) {
+		logger.info("replyDelete 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("replyDelete", pMap);
+		return result;
+	}
+
+	public int report(Map<String, Object> pMap) {
+		logger.info("report 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("report", pMap);
+		return result;
+	}
+
+	public int likeOn(Map<String, Object> pMap) {
+		logger.info("likeOn 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("likeOn", pMap);
+		return result;
+	}
+
+	public int likeOff(Map<String, Object> pMap) {
+		logger.info("likeOff 호출");
+		int result = 0;
+		// result = sqlSessionTemplate.update("likeOff", pMap);
+		return result;
+	}
 }
--- backend/src/main/java/com/backend/itryp/logic/BoardLogic.java
+++ backend/src/main/java/com/backend/itryp/logic/BoardLogic.java
@@ -1,5 +1,111 @@
 package com.backend.itryp.logic;
 
-public class BoardLogic {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backend.itryp.dao.BoardDao;
+
+@Service
+public class BoardLogic {
+	Logger logger = LogManager.getLogger(BoardLogic.class);
+	
+	@Autowired
+	private BoardDao boardDao = null;
+
+	public List<Map<String, Object>> boardList(Map<String, Object> pMap) {
+		logger.info("boardList 호출");
+		List<Map<String,Object>> bList = new ArrayList<>();
+		bList= boardDao.boardList(pMap);
+		return bList;
+	}
+
+	public List<Map<String, Object>> boardDetail(Map<String, Object> pMap) {
+		logger.info("boardDetail 호출");
+		List<Map<String,Object>> bList = new ArrayList<>();
+		bList= boardDao.boardDetail(pMap);
+		return bList;
+	}
+
+	public List<Map<String, Object>> boardHot(Map<String, Object> pMap) {
+		logger.info("boardHot 호출");
+		List<Map<String,Object>> bList = new ArrayList<>();
+		bList= boardDao.boardHot(pMap);
+		return bList;
+	}
+
+	public int boardInsert(Map<String, Object> pMap) {
+		logger.info("boardInsert 호출");
+		int result = 0;
+		result = boardDao.boardInsert(pMap);
+		return result;
+	}
+
+	public int boardUpdate(Map<String, Object> pMap) {
+		logger.info("boardUpdate 호출");
+		int result = 0;
+		result = boardDao.boardUpdate(pMap);
+		return result;
+	}
+
+	public int boardDelete(Map<String, Object> pMap) {
+		logger.info("boardDelete 호출");
+		int result = 0;
+		result = boardDao.boardDelete(pMap);
+		return result;
+	}
+
+	public List<Map<String, Object>> replyList(Map<String, Object> pMap) {
+		logger.info("replyList 호출");
+		List<Map<String,Object>> bList = new ArrayList<>();
+		bList= boardDao.replyList(pMap);
+		return bList;
+	}
+
+	public int replyInsert(Map<String, Object> pMap) {
+		logger.info("replyInsert 호출");
+		int result = 0;
+		result = boardDao.replyInsert(pMap);
+		return result;
+	}
+
+	public int replyUpdate(Map<String, Object> pMap) {
+		logger.info("replyUpdate 호출");
+		int result = 0;
+		result = boardDao.replyUpdate(pMap);
+		return result;
+	}
+
+	public int replyDelete(Map<String, Object> pMap) {
+		logger.info("replyDelete 호출");
+		int result = 0;
+		result = boardDao.replyDelete(pMap);
+		return result;
+	}
+
+	public int report(Map<String, Object> pMap) {
+		logger.info("report 호출");
+		int result = 0;
+		result = boardDao.report(pMap);
+		return result;
+	}
+
+	public int likeOn(Map<String, Object> pMap) {
+		logger.info("likeOn 호출");
+		int result = 0;
+		result = boardDao.likeOn(pMap);
+		return result;
+	}
+
+	public int likeOff(Map<String, Object> pMap) {
+		logger.info("likeOff 호출");
+		int result = 0;
+		result = boardDao.likeOff(pMap);
+		return result;
+	}
 }
