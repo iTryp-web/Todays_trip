a0218d93d80202b5c9f45ef3c9241c95c1c4b467
EJ <temp221128@gmail.com>
2023-03-29 23:24:52 +0900

    03/29 OrderPage update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import TestPage from './components/page/TestPage';
 import HomePage from './components/page/HomePage';
 import BoardPage from './components/page/BoardPage';
+import OrderPage from './components/page/OrderPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' exact={true} element={<HomePage />} />
       <Route path='/community' exact={true} element={<BoardPage />} />
       <Route path='/test' exact={true} element={<TestPage />} />
+      <Route path='/order' exact={true} element={<OrderPage />} />
     </Routes>
     </> 
   );
