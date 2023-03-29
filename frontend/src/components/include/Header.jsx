ffc3ef58d557286721b0ff77493603aac9c19334
jungyunmok <jungyunmok@gmail.com>
2023-03-29 22:10:44 +0900

    03/29 Header설정 및 board 수정

--- frontend/src/components/include/Header.jsx
+++ frontend/src/components/include/Header.jsx
@@ -1,25 +1,62 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { NavLinkStyle, HeaderContentsBlock } from '../../styles/HeaderStyle';
+import { MdSearch } from "react-icons/md";
+import { BsCart } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
+  const menuClick = () => {
+
+  }
+  
   return (
-    <React.Fragment>
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#home">JavaCamp</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Link to="/" className="nav-link">Home</Link>
-              <Link to="/test" className="nav-link">테스트</Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </React.Fragment>
+    <>
+      <HeaderContentsBlock>
+      <AiOutlineMenu className="menu-icon" onClick={() => menuClick()} />
+        <Link to="/" className="logo">
+          오늘의 여행
+        </Link>
+        <nav>
+          <NavLinkStyle 
+            to="/market"
+            className={({ isActive }) => isActive && "active"}
+          >
+            마켓
+          </NavLinkStyle>
+          <NavLinkStyle
+            to="/community"
+            className={({ isActive }) => isActive && "active"}
+          >
+            커뮤니티
+          </NavLinkStyle>
+          <NavLinkStyle
+            to="/experts"
+            className={({ isActive }) => isActive && "active"}
+          >
+            고객센터
+          </NavLinkStyle>
+        </nav>
+        <div className="button-block">
+          <div className="search">
+            <MdSearch className="search-icon" />
+            <input className="search-input" placeholder="검색" />
+          </div>
+          <Link to="cart" className="cart">
+            <BsCart className="cart-icon" />
+          </Link>
+          <div className="buttons">
+            <Link to="login" className="login button">
+              로그인
+            </Link>
+            <Link to="register" className="register button">
+              회원가입
+            </Link>
+            <button className="seller-button">판매자 가입</button>
+          </div>
+        </div>
+      </HeaderContentsBlock>
+    </>
   )
 }
 
