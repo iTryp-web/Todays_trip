383d32c766d8f0a1ee03a3619d02c2080bf395fe
EJ <temp221128@gmail.com>
2023-04-01 10:49:27 +0900

    04/01 orderResult page update

--- frontend/src/components/order/OrderSuccess.jsx
+++ frontend/src/components/order/OrderSuccess.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 import Bottom from '../include/Bottom'
 import Header from '../include/Header'
 import check_icon from '../../images/check.png'
-import './orderSuccess.css'
-import { Button } from 'react-bootstrap'
+import './orderResult.css'
 
 const OrderSuccess = () => {
   return (
     <>
       <Header/>
         <div className='order body'>
+          <div className='complete sentence'>주문이 완료되었습니다.</div>
+          <div className='img'><img src={check_icon} alt="check_icon" className='check icon'/></div>
+          <div className='num'>주문 번호 : 어쩌고저쩌고</div>
           <hr/>
-          <div className='complete_sentence'>주문이 완료되었습니다.</div>
-          <div className='img'><img src={check_icon} alt="check_icon" className='check_icon'/></div>
-          {/* <BButon */}
-          <Button>주문 내역</Button>&nbsp;&nbsp;
-          <Button>홈으로</Button>
+          <button className='order button'>주문 내역</button>
+          <button className='order button home'>메인으로</button>
           <hr/>
         </div>
       <Bottom/>
