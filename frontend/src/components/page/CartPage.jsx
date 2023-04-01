50051b1a22e0ae60a2b796214cb000fe27a7b8cc
EJ <temp221128@gmail.com>
2023-04-01 10:49:27 +0900

    04/01 orderResult page update

--- frontend/src/components/page/CartPage.jsx
+++ frontend/src/components/page/CartPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../include/Header'
+import Bottom from '../include/Bottom'
 
 const CartPage = () => {
   return (
     <>
-      
+      <Header/>
+      cart
+      <Bottom/>
     </>
   )
 }
