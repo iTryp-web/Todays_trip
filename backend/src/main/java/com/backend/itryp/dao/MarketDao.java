2a73244cae42711959affd93a0f57f7219a44bc5
dodoringring <dodohee1504@gmail.com>
2023-04-03 21:30:07 +0900

    MarketController 수정

--- backend/src/main/java/com/backend/itryp/dao/MarketDao.java
+++ backend/src/main/java/com/backend/itryp/dao/MarketDao.java
@@ -1,8 +1,21 @@
 package com.backend.itryp.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MarketDao {
+
+	public List<Map<String, Object>> marketList(Map<String, Object> pMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Map<String, Object>> marketDetail(Map<String, Object> pMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
