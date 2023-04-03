4ad6541b2cb41cb4fa15ec632dea10026f76a73c
dodoringring <dodohee1504@gmail.com>
2023-04-03 21:30:07 +0900

    MarketController 수정

--- backend/src/main/java/com/backend/itryp/logic/MarketLogic.java
+++ backend/src/main/java/com/backend/itryp/logic/MarketLogic.java
@@ -1,5 +1,6 @@
 package com.backend.itryp.logic;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,26 +9,44 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.backend.itryp.dao.BoardDao;
 import com.backend.itryp.dao.MarketDao;
 
 @Service
 public class MarketLogic {
-	Logger logger = LogManager.getLogger(MarketLogic.class);
+	 Logger logger = LogManager.getLogger(MarketLogic.class);
 	
 	@Autowired
-	private MarketDao marketDao = null;
+	private static MarketDao marketDao = null;
 
-	public static List<Map<String, Object>> marketList(Map<String, Object> pMap) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 마켓 리스트
+	 * 
+	 * @param pMap
+	 * @return
+	 */
+	public List<Map<String, Object>> marketList(Map<String, Object> pMap) {
+		logger.info("marketList 호출");
+		List<Map<String,Object>> bList = new ArrayList<>();
+		bList= marketDao.marketList(pMap);
+		return bList;
 	}
-
+	/**
+	 * 마켓글 상세조회
+	 * 
+	 * @param pMap
+	 * @return
+	 */
 	public List<Map<String, Object>> marketDetail(Map<String, Object> pMap) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("boardDetail 호출");
+		List<Map<String,Object>> bList = new ArrayList<>();
+		bList= marketDao.marketDetail(pMap);
+		return bList;
 	}
-
+	/**
+	 * 마켓 - Quill image -> 추후 수정 필요(파일이름, url 처리방법)!!
+	 * @param pMap
+	 * @return
+	 */
 	public int marketUpdate(Map<String, Object> pMap) {
 		// TODO Auto-generated method stub
 		return 0;
@@ -54,6 +73,26 @@ public class MarketLogic {
 	}
 
 	public int reviewLike(Map<String, Object> pMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int qnaDelete(Map<String, Object> pMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int qnaReplyInsert(Map<String, Object> pMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int qnaInsert(Map<String, Object> pMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int marketInsert(Map<String, Object> pMap) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
