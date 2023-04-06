41b5b68153bf639d56fd03ff32cb9613248f92bf
aypd02150@gmail.com <aypd02150@gmail.com>
2023-04-06 22:19:03 +0900

    04/06 member back완성?

--- backend/src/main/java/com/backend/itryp/dao/MemberDao.java
+++ backend/src/main/java/com/backend/itryp/dao/MemberDao.java
@@ -16,23 +16,38 @@ public class MemberDao {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate = null;
 	
+	//회원정보 조회, 로그인, 회원정보 조회할때 비밀번호 체크용
 	public List<Map<String,Object>> memberList(Map<String,Object> pMap){
 		logger.info("memberList 호출");
 		List<Map<String,Object>> mList = sqlSessionTemplate.selectList("memberList", pMap);
 		return mList;
 	}
-
+	//중복검사, 이메일 찾기
+	public List<Map<String, Object>> checkInfo(Map<String, Object> pMap) {
+		logger.info("checkInfo 호출");
+		List<Map<String, Object>> mList = sqlSessionTemplate.selectList("checkInfo",pMap);
+		
+		return mList;
+	}
+	//회원가입
 	public int memberInsert(Map<String, Object> pMap) {
 		logger.info("memberInsert 호출");
 		int result = 0;
 		result = sqlSessionTemplate.update("memberInsert",pMap);
 		return result;
 	}
-
+	//회원정보 수정
 	public int memberUpdate(Map<String, Object> pMap) {
 		logger.info("memberUpdate 호출");
 		int result = 0;
 		result = sqlSessionTemplate.update("memberUpdate",pMap);
+		return result;
+	}
+	//회원 탈퇴
+	public int memberDelete(Map<String, Object> pMap) {
+		logger.info("memberDelete 호출");
+		int result = 0;
+		result = sqlSessionTemplate.update("memberDelete",pMap);
 		return result;
 	}
 }
