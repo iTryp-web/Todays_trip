0caaa82cb6079006ff7154763be7d92be5773eb3
77EJ77 <temp221128@gmail.com>
2023-04-13 17:43:25 +0900

    04/13 order ui update

--- frontend/src/components/page/OrderPage.jsx
+++ frontend/src/components/page/OrderPage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../include/Header'
 import OrderRow from '../order/OrderRow'
 import Footer from '../include/Footer'
-import { OrderDiv, OrderTitle, LineHr, OrderListDiv, OrderInfoDiv, OrderAddressDiv, OrderCouponDiv, PaymentButton, OrderButtonDiv, OrdererInfoDiv, OrdertyTitle, SelectList } from '../../styles/OrderStyle'
+import { OrderDiv, OrderTitle, LineHr, OrderListDiv, OrderAddressDiv, OrderCouponDiv, PaymentButton, OrderButtonDiv, OrdererInfoDiv, OrdertyTitle, SelectList, OrdertysTitle, PointContent, OrderCalcDiv, OrdererTable, ConfirmButton, OrdererTd, OrdererTytd, OrderCalcTyDiv, OrderCalcListDiv, OrderCalcResultDiv } from '../../styles/OrderStyle'
 
 const OrderPage = () => {
 
-  function onClickPayment() {
+  const couponList = null;
+
+  const onClickPayment = () => {
     const { IMP } = window;
     IMP.init(process.env.REACT_APP_IMPORT_INIT_KEY);
     
@@ -45,30 +47,65 @@ const OrderPage = () => {
       <Header/>
       <OrderDiv>
         <OrderTitle>예약하기</OrderTitle>
-        <LineHr/>
         <OrderListDiv>
           <OrdertyTitle>주문 정보</OrdertyTitle>
+          <LineHr/>
           <OrderRow/>
         </OrderListDiv>
         <OrderCouponDiv>
           <OrdertyTitle>할인 정보</OrdertyTitle>
-          쿠폰 할인<br/>
+          <LineHr/>
+          <OrdertysTitle>쿠폰 할인</OrdertysTitle>
           <SelectList>
+            {couponList ? 
+            <option>사용 가능한 쿠폰이 존재하지 않습니다.</option>
+            : 
             <option>쿠폰1</option>
-            <option>쿠폰2</option>
-            <option>쿠폰3</option>
-            <option>쿠폰4</option>
+            }
           </SelectList><br/>
-          포인트
+          <OrdertysTitle>포인트</OrdertysTitle>
+          <PointContent>사용 가능 포인트 {0} 점</PointContent>
+          <span><input type="decimal" placeholder="" disabled="" pattern="[0-9]*" class="css-o9jzai" value="0"></input></span><span><ConfirmButton>모두 사용</ConfirmButton></span>
         </OrderCouponDiv>
         <OrdererInfoDiv>
-          <OrdertyTitle>주문자 정보</OrdertyTitle>
+          <OrdertyTitle>예약자 정보</OrdertyTitle>
+          <LineHr/>
+          <OrdererTable>
+            <tr>
+              <OrdererTytd>예약자 이름</OrdererTytd>
+              <td>{"둘리"}</td>
+              <table>
+                <tr>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </table><OrdererTd rowSpan={3}><ConfirmButton>정보 변경</ConfirmButton></OrdererTd>
+            </tr>
+            <tr>
+              <OrdererTytd>예약자 연락처</OrdererTytd>
+              <td>{"요리보고"}</td> 
+            </tr>
+            <tr>
+              <OrdererTytd>예약자 이메일</OrdererTytd>
+              <td>{"조리봐도"}</td> 
+            </tr>
+          </OrdererTable>
           </OrdererInfoDiv>
         <OrderAddressDiv>
           <OrdertyTitle>배송지 정보</OrdertyTitle>
-          </OrderAddressDiv>
+          <LineHr/>
+        </OrderAddressDiv>
+        <OrderCalcDiv>
+          <OrdertyTitle>결제 정보</OrdertyTitle>
+          <OrderCalcTyDiv>
+            <OrderCalcListDiv>주문 금액 {1111} 원 - 할인 금액 {0} 원</OrderCalcListDiv>
+            <OrderCalcResultDiv>
+              <span>결제 금액</span><span style={{margin: "left: 50%"}}>{1111}</span>원
+            </OrderCalcResultDiv>
+          </OrderCalcTyDiv>
+        </OrderCalcDiv>
+        <OrdertyTitle>결제 동의</OrdertyTitle>
         <OrderButtonDiv>
-          결제 동의 약관<br/>
           <PaymentButton onClick={onClickPayment}>결제하기</PaymentButton>
         </OrderButtonDiv>
       </OrderDiv>
--- frontend/src/styles/OrderStyle.js
+++ frontend/src/styles/OrderStyle.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const OrderDiv = styled.div`
   min-height: 0;
   margin-top: 1%;
-  padding: 0 7%;
+  padding: 0 15%;
   height: 100vh;
 `;
 
@@ -11,13 +11,43 @@ export const OrderListDiv = styled.div`
   margin-top: 3%;
 `;
 
-export const OrderInfoDiv = styled.div``;
+export const OrderInfoDiv = styled.div`
+  margin-bottom: 3%;
+`;
 
-export const OrderCouponDiv = styled.div``;
+export const OrderCouponDiv = styled.div`
+  margin-bottom: 3%;
+`;
 
-export const OrderAddressDiv = styled.div``;
+export const OrderAddressDiv = styled.div`
+  margin-bottom: 3%;
+`;
 
-export const OrdererInfoDiv = styled.div``;
+export const OrdererInfoDiv = styled.div`
+  margin-bottom: 3%;
+`;
+
+export const OrderCalcDiv = styled.div`
+  margin-bottom: 3%;
+`;
+
+export const OrderCalcTyDiv = styled.div`
+  margin-top: 1.5%;
+  margin-bottom: 3%;
+  background-color: #f5f5f5;
+  padding: 1% 2%;
+  @media screen and (max-width: 768px) {
+    padding: 2% 3%;
+  }
+`;
+
+export const OrderCalcListDiv = styled.div`
+  font-size: small;
+`;
+
+export const OrderCalcResultDiv = styled.div`
+  font-weight: bold;
+`;
 
 export const OrderButtonDiv = styled.div`
   text-align: center;
@@ -25,6 +55,7 @@ export const OrderButtonDiv = styled.div`
 
 export const OrderTitle = styled.div`
   margin-top: 3%;
+  margin-bottom: 3%;
   font-size: 1.5rem;
   font-weight: bold;
 `;
@@ -39,6 +70,73 @@ export const OrdertyTitle = styled.div`
   font-weight: bold;
 `;
 
+export const OrdertysTitle = styled.div`
+  margin-top: 1em;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const OrdererTable = styled.table`
+  width: 100%;
+`;
+
+export const OrdererTytd = styled.td`
+  @media screen and (max-width: 700px) {
+    width: 120px;
+  }
+  width: 170px;
+  font-size: 15px;
+  white-space: nowrap;
+`;
+
+export const OrdererTd = styled.td`
+  align-items: center;
+  text-align: center;
+`;
+
+export const PointButton = styled.button`
+  padding: 0 10px;
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border: 0px;
+  border-radius: 4px;
+  font-weight: bold;
+  letter-spacing: -0.2px;
+  transition: all 0.3s ease 0s;
+  user-select: none;
+  outline-style: none;
+  cursor: pointer;
+  height: 40px;
+  font-size: 14px;
+  background-color: #e9ecef;
+`;
+
+export const ConfirmButton = styled.button`
+  padding: 0 10px;
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border: 0px;
+  border-radius: 4px;
+  font-weight: bold;
+  letter-spacing: -0.2px;
+  transition: all 0.3s ease 0s;
+  user-select: none;
+  outline-style: none;
+  cursor: pointer;
+  height: 40px;
+  font-size: 14px;
+  background-color: #e9ecef;
+  color: #495056;
+`;
+
 export const PaymentButton = styled.button`
   border: none;
   background-color: #4996f3;
@@ -46,9 +144,18 @@ export const PaymentButton = styled.button`
   padding: 7px 20px;
   border-radius: 5px;
   margin-top: 5%;
+  margin-bottom: 10%;
+  &hover: {
+    cursor: pointer;
+    opacity: 0.8;
+  }
 `;
 
 export const SelectList = styled.select`
   border-radius: 4px;
-  width: 200px;
+  margin: 1% 0;
+  width: 320px;
+  padding: 1%;
 `;
+
+export const PointContent = styled.div``;
