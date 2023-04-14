3883f7130a813f0576840805fc8b120bf94b9495
dodoringring <dodohee1504@gmail.com>
2023-04-14 17:47:16 +0900

    04/14 마켓

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import BoardDetail from './components/board/BoardDetail';
 import SignUpPage from './components/page/SignUpPage';
 import SignInPage from './components/page/SignInPage';
 import FaqPage from './components/page/FaqPage';
+import MarketWrite from './components/market/MarketWrite';
 
 
 
@@ -28,12 +29,12 @@ function App() {
       <Route path='/test' exact={true} element={<TestPage />} />
       <Route path='/cart' exact={true} element={<CartPage />} />
       <Route path='/order' exact={true} element={<OrderPage />} />
-      <Route path='/market' exact={true} element={<MarketPage />} />
       <Route path='/order/success' exact={true} element={<OrderSuccess />} />
       <Route path='/order/fail' exact={true} element={<OrderFail />} />
       <Route path='/signup' exact={true} element={<SignUpPage />} />
       <Route path='/signin' exact={true} element={<SignInPage />} />
       <Route path='/market' exact={true} element={<MarketPage />} />
+      <Route path='/market/write' exact={true} element={<MarketWrite />} />
       <Route path='/Support' exact={true} element={<FaqPage />} />
     </Routes>
     </> 
