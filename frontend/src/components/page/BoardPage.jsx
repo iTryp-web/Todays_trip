bdd4a299c948561f3c1bfda861c4cec869f0d3ec
jungyunmok <jungyunmok@gmail.com>
2023-04-16 23:46:47 +0900

    04/16 커뮤니티 완성(신고, 댓글 수정 추가)

--- frontend/src/components/page/BoardPage.jsx
+++ frontend/src/components/page/BoardPage.jsx
@@ -7,17 +7,27 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import BoardLayout from '../board/BoardLayout';
 
 const BoardPage = () => {
+  // 단위테스트용!!
+  window.sessionStorage.setItem('user_id', 'test1')
+  window.sessionStorage.setItem('user_nickname', '테스트1')
   const navigate = useNavigate()
+
+  // 로그인할때 세션스토리지에 담았다가 꺼낼 것!
+  // 아이디, 닉네임 담을 변수
+  const [userId] = useState(window.sessionStorage.getItem('user_id'))
+  const [userNickname] = useState(window.sessionStorage.getItem('user_nickname'))
 
   return (
     <>
       <Header />
       <BoardHeader>
         <BoardH3>커뮤니티</BoardH3>
+        {userId ? (
         <BtnWrite onClick={() => navigate('/board/write')}>
           글쓰기
           <RiPencilFill />
         </BtnWrite>
+        ) : null}
       </BoardHeader>
       <BoardLayout />
       <Footer />
