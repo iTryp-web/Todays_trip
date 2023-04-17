261bf5668f4fa766a4598f636cb7261f434d8a81
unknown <wtsc790@gmail.com>
2023-04-17 22:41:07 +0900

    faq navBar updating

--- frontend/src/components/support/FaqNav.jsx
+++ frontend/src/components/support/FaqNav.jsx
@@ -1,20 +1,58 @@
-import React from 'react'
-import { faqNavStyle } from "../../styles/SupportStyle";
+import React, { useState } from "react";
+import { fnDefaultLabel, fnStyle } from "../../styles/SupportStyle";
+import styled from "styled-components";
 
+const NavWrapper = styled.div`
+  ${fnStyle}
+`;
 
 const FaqNav = () => {
+  const [selectedLabel, setSelectedLabel] = useState("전체");
+
+  const handleLabelClick = (e) => {
+    const label = e.target.textContent;
+    setSelectedLabel(label);
+  };
+
+
+
   return (
     <>
-    <div>
-      <nav>
-        <label className="fnLabel" htmlFor="전체">
-          <input type="checkbox" />
-          <span>전체</span>
-        </label>
-      </nav>
-    </div>
-    </> 
-  )
-}
+      <NavWrapper>
+        <nav>
+          <label
+            className={selectedLabel === "전체" ? "fnSelectedLabel" : "fnLabel"}
+            htmlFor="전체"
+          >
+            <input id="전체" type="checkbox" className="fnInput" name="fnInput" />
+            <span className="fnSpan" onClick={handleLabelClick}>
+              전체
+            </span>
+          </label>
+          <label
+            className={
+              selectedLabel === "환불" ? "fnSelectedLabel" : "fnLabel"
+            }
+            htmlFor="환불"
+          >
+            <input id="환불" type="checkbox" className="fnInput" name="fnInput"/>
+            <span className="fnSpan" onClick={handleLabelClick}>
+              환불
+            </span>
+          </label>
+          <label
+            className={selectedLabel === "결제" ? "fnSelectedLabel" : "fnLabel"}
+            htmlFor="결제"
+          >
+            <input id="결제" type="checkbox" className="fnInput" name="fnInput"/>
+            <span className="fnSpan" onClick={handleLabelClick}>
+              결제
+            </span>
+          </label>
+        </nav>
+      </NavWrapper>
+    </>
+  );
+};
 
 export default FaqNav;
