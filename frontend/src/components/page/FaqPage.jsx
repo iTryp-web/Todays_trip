f0b3c6501efadbaacf4fa1e6a0612e3830582f7d
cvrts <wtsc790@gmail.com>
2023-04-17 09:46:11 +0900

    support front updated

--- frontend/src/components/page/FaqPage.jsx
+++ frontend/src/components/page/FaqPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import CsSection from "../support/CsSection";
-import Header from '../include/Header'
-import Footer from '../include/Footer';
+import Header from "../include/Header";
+import Footer from "../include/Footer";
+import FaqSection from "../support/FaqSection";
+import FaqNav from "../support/FaqNav";
 
 const FaqPage = () => {
   return (
     <>
       <Header />
       <CsSection />
+      <FaqNav />
+      <FaqSection />
       <Footer />
     </>
   );
