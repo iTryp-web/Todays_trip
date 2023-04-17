78571d0f27c676bdcb49b1b6f6e231f42364ecd0
unknown <wtsc790@gmail.com>
2023-04-18 02:36:55 +0900

    support front updated

--- frontend/src/components/support/CsSection.jsx
+++ frontend/src/components/support/CsSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiOutlinePhone } from "react-icons/ai";
 import { BsDot } from "react-icons/bs";
-import { CustomerService } from "../../styles/SupportStyle";
+import { CustomerService, QMark } from "../../styles/SupportStyle";
 
 const csSection = () => {
   return (
@@ -9,42 +9,42 @@ const csSection = () => {
       <CustomerService>
         <section className="csSection">
           <div className="innerStyle">
-            <div className="helpDiv" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <div className="helpDiv" style={{ display: "flex", flexDirection: "column" }}>
               <h2 className="helpTitle">무엇을 도와드릴까요?</h2>
               <ul className="helpUl">
                 <li className="helpLi">
-                  <span className="qMark">
+                  <QMark>
                     Q
-                  </span>{" "}
+                  </QMark>{" "}
                   결제는 어떻게 취소하나요?
                 </li>
                 <li className="helpLi">
-                  <span className="qMark">
+                  <QMark>
                     Q
-                  </span>{" "}
+                  </QMark>{" "}
                   비밀번호가 기억나지 않습니다.
                 </li>
                 <li className="helpLi">
-                  <span className="qMark">
+                  <QMark>
                     Q
-                  </span>{" "}
+                  </QMark>{" "}
                   제품을 더 자세히 알고싶어요.
                 </li>
                 <li className="helpLi">
-                  <span className="qMark">
+                  <QMark>
                     Q
-                  </span>{" "}
+                  </QMark>{" "}
                   판매자 등록은 얼마나 걸리나요?
                 </li>
                 <li className="helpLi">
-                  <span className="qMark">
+                  <QMark>
                     Q
-                  </span>{" "}
+                  </QMark>{" "}
                   환불 금액이 입금되지 않습니다.
                 </li>
               </ul>
             </div>
-            <div className="csDiv" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <div className="csDiv" style={{ display: "flex", flexDirection: "column"}}>
               <h2 className="csTitle">
                 고객센터 <span className="time">09:00 ~ 18:00</span>
               </h2>
