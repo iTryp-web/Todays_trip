57c22aeb733b79c89d5edef785a85880ea6a6c48
77EJ77 <temp221128@gmail.com>
2023-04-20 17:46:47 +0900

    04/20 vo update

--- backend/src/main/java/com/backend/itryp/vo/TbUserVO.java
+++ backend/src/main/java/com/backend/itryp/vo/TbUserVO.java
@@ -14,6 +14,7 @@ public class TbUserVO {
 	private String user_id;
 	private String user_pw;
 	private String user_nickname;
+	private String user_email;
 	private String user_name;
 	private String user_phone;
 	private String user_zipcode;
