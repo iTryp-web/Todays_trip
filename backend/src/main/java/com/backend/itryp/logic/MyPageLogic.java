e1c98d36d7a6a765a3db8a0fbd368d9e41f1164e
EJ <temp221128@gmail.com>
2023-04-25 00:06:47 +0900

    04/24 myPage getMyBoard update

--- backend/src/main/java/com/backend/itryp/logic/MyPageLogic.java
+++ backend/src/main/java/com/backend/itryp/logic/MyPageLogic.java
@@ -1,5 +1,9 @@
 package com.backend.itryp.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +18,71 @@ public class MyPageLogic {
 	
 	@Autowired
 	MyPageDao mpDao = null;
+
+	
+	/**
+	 * getMyBoardList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 글 목록
+	 */
+	public List<Map<String, Object>> getMyBoardList(Map<String, Object> pmap) {
+		log.info("getMyBoardList 호출");
+		List<Map<String, Object>> rList = new ArrayList<>();
+		rList = mpDao.getMyBoardList(pmap);
+		return rList;
+	}
+
+	/**
+	 * getMyRepleList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 댓글 목록
+	 */
+	public List<Map<String, Object>> getMyRepleList(Map<String, Object> pmap) {
+		log.info("getMyRepleList 호출");
+		List<Map<String, Object>> rList = new ArrayList<>();
+		rList = mpDao.getMyRepleList(pmap);
+		return rList;
+	}
+
+	/**
+	 * getMyReviewList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 리뷰 목록
+	 */
+	public List<Map<String, Object>> getMyReviewList(Map<String, Object> pmap) {
+		log.info("getMyReviewList 호출");
+		List<Map<String, Object>> rList = new ArrayList<>();
+		rList = mpDao.getMyReviewList(pmap);
+		return rList;
+	}
+
+	/**
+	 * getMyLikeList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 좋아요 목록
+	 */
+	public List<Map<String, Object>> getMyLikeList(Map<String, Object> pmap) {
+		log.info("getMyLikeList 호출");
+		List<Map<String, Object>> rList = new ArrayList<>();
+		rList = mpDao.getMyLikeList(pmap);
+		return rList;
+	}
+	
+	/**
+	 * getMyQNAList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 문의글 목록
+	 */
+	public List<Map<String, Object>> getMyQNAList(Map<String, Object> pmap) {
+		log.info("getMyQNAList 호출");
+		List<Map<String, Object>> rList = new ArrayList<>();
+		rList = mpDao.getMyQNAList(pmap);
+		return rList;
+	}
 
 }
