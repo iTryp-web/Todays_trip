4413a350c539fefba5e86b98f17e90ddd5aa49a5
EJ <temp221128@gmail.com>
2023-04-25 00:06:47 +0900

    04/24 myPage getMyBoard update

--- backend/src/main/java/com/backend/itryp/controller/MyPageController.java
+++ backend/src/main/java/com/backend/itryp/controller/MyPageController.java
@@ -1,12 +1,18 @@
 package com.backend.itryp.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.itryp.logic.MyPageLogic;
+import com.google.gson.Gson;
 
 @RestController
 @RequestMapping("/mypage/*")
@@ -17,5 +23,90 @@ public class MyPageController {
 	@Autowired
 	MyPageLogic mpLogic = null;
 	
+	
+	/**
+	 * 커뮤니티 내 글 불러오기
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 글 목록
+	 */
+	@PostMapping("boardList")
+	public String getMyBoardList(@RequestBody Map<String, Object> pmap) {
+		log.info("getMyBoardList 호출 " + pmap);
+		String temp = null;
+		List<Map<String,Object>> rmap = null;
+		rmap = mpLogic.getMyBoardList(pmap);
+		Gson g = new Gson();
+		temp = g.toJson(rmap);
+		return temp;
+	}
+	
+	/**
+	 * 커뮤니티 내 댓글 불러오기
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 댓글 목록
+	 */
+	@PostMapping("repleList")
+	public String getMyRepleList(@RequestBody Map<String, Object> pmap) {
+		log.info("getMyRepleList 호출 " + pmap);
+		String temp = null;
+		List<Map<String,Object>> rmap = null;
+		rmap = mpLogic.getMyRepleList(pmap);
+		Gson g = new Gson();
+		temp = g.toJson(rmap);
+		return temp;
+	}
+	
+	/**
+	 *  내가 작성한 리뷰글 불러오기
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 리뷰 목록
+	 */
+	@PostMapping("reviewList")
+	public String getMyReviewList(@RequestBody Map<String, Object> pmap) {
+		log.info("getMyReviewList 호출 " + pmap);
+		String temp = null;
+		List<Map<String,Object>> rmap = null;
+		rmap = mpLogic.getMyReviewList(pmap);
+		Gson g = new Gson();
+		temp = g.toJson(rmap);
+		return temp;
+	}
+	
+	/**
+	 *  내가 좋아요한 글 불러오기
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 좋아요 목록
+	 */
+	@PostMapping("likeList")
+	public String getMyLikeList(@RequestBody Map<String, Object> pmap) {
+		log.info("getMyLikeList 호출 " + pmap);
+		String temp = null;
+		List<Map<String,Object>> rmap = null;
+		rmap = mpLogic.getMyLikeList(pmap);
+		Gson g = new Gson();
+		temp = g.toJson(rmap);
+		return temp;
+	}
+	
+	/**
+	 *  내가 작성한 QNA 글 불러오기
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 문의글 목록
+	 */
+	@PostMapping("qnaList")
+	public String getMyQNAList(@RequestBody Map<String, Object> pmap) {
+		log.info("getMyQNAList 호출 " + pmap);
+		String temp = null;
+		List<Map<String,Object>> rmap = null;
+		rmap = mpLogic.getMyQNAList(pmap);
+		Gson g = new Gson();
+		temp = g.toJson(rmap);
+		return temp;
+	}
 	
 }
--- backend/src/main/java/com/backend/itryp/dao/MyPageDao.java
+++ backend/src/main/java/com/backend/itryp/dao/MyPageDao.java
@@ -1,5 +1,8 @@
 package com.backend.itryp.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -13,4 +16,70 @@ public class MyPageDao {
 	
 	@Autowired
 	private SqlSessionTemplate sst = null;
+
+	
+	/**
+	 * getMyBoardList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 글 목록
+	 */
+	public List<Map<String, Object>> getMyBoardList(Map<String, Object> pmap) {
+		log.info("getMyBoardList 호출");
+		List<Map<String, Object>> rList = null;
+		rList = sst.selectList("getMyBoardList", pmap);
+		return rList;
+	}
+
+	/**
+	 * getMyRepleList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 댓글 목록
+	 */
+	public List<Map<String, Object>> getMyRepleList(Map<String, Object> pmap) {
+		log.info("getMyRepleList 호출");
+		List<Map<String, Object>> rList = null;
+		rList = sst.selectList("getMyRepleList", pmap);
+		return rList;
+	}
+
+	/**
+	 * getMyReviewList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 리뷰 목록
+	 */
+	public List<Map<String, Object>> getMyReviewList(Map<String, Object> pmap) {
+		log.info("getMyReviewList 호출");
+		List<Map<String, Object>> rList = null;
+		rList = sst.selectList("getMyReviewList", pmap);
+		return rList;
+	}
+
+	/**
+	 * getMyLikeList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 좋아요 목록
+	 */
+	public List<Map<String, Object>> getMyLikeList(Map<String, Object> pmap) {
+		log.info("getMyLikeList 호출");
+		List<Map<String, Object>> rList = null;
+		rList = sst.selectList("getMyLikeList", pmap);
+		return rList;
+	}
+
+	/**
+	 * getMyQNAList
+	 * 
+	 * @param pmap 유저 정보
+	 * @return rList 문의글 목록
+	 */
+	public List<Map<String, Object>> getMyQNAList(Map<String, Object> pmap) {
+		log.info("getMyQNAList 호출");
+		List<Map<String, Object>> rList = null;
+		rList = sst.selectList("getMyQNAList", pmap);
+		return rList;
+	}
 }
