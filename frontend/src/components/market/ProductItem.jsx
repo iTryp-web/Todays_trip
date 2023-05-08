38e92d6599f776a723edf2a59bc78f5eba146b2a
dodoringring <dodohee1504@gmail.com>
2023-05-08 18:05:55 +0900

    5/08 marketList 구조 고침

--- frontend/src/components/market/ProductItem.jsx
+++ frontend/src/components/market/ProductItem.jsx
@@ -7,31 +7,40 @@ import { ContentsBlock, ItemBlock, Star } from '../../styles/MarketStyle';
 
 
 
-const ProductItem = ({items}) => {
+const ProductItem = ({item}) => {
 
   
   const navigate=useNavigate()
- 
+   console.log(item)
+  console.log(item.STAR_AVG)//1건
+
+//  // 별점
+//  let star=item.star_avg;
+
+//  //star rating percentage 계산 후 style로 반영
+//  const ratingToPercent = {
+//    width: `${(star / 5) * 100}%`,
+//  };
 
 
   return (
     <>
-      <ContentsBlock>
-        {items && items.map((item,index)=>(
-          <ItemBlock onClick={() => navigate('/market/detail/'+item.market_no)}>
-            <img className='image' src={item.file_url} />
+      {/* <ContentsBlock> */}
+        {item && (
+          <ItemBlock onClick={() => navigate('/market/detail/'+item.MARKET_NO)}>
+            <img className='image' src={item.FILE_URL} />
             <div className="body">
-              <span className="category">{item.market_category}</span>
-              <span className="title">{item.market_title}</span>
+              <span className="category">{item.MARKET_CATEGORY}</span>
+              <span className="title">{item.MARKET_TITLE}</span>
              {/* 할인률 줘도되고 안줘도되고 */}
               {/* <span className="discount">20%</span> */}
-              <span className="price">{item.market_price}</span>
+              <span className="price">{item.MARKET_PRICE}</span>
 
 
             
               <Star>
               <div className="star_rating">
-                <div className="star_rating_fill" style={{width:`${(item.star_avg?item.star_avg:0 / 5) * 100}%`}} >
+                <div className="star_rating_fill" style={{width:`${((item.STAR_AVG?item.STAR_AVG:0 )/ 5) * 100}%`}} >
       
                   
                   <span>★</span>
@@ -49,11 +58,11 @@ const ProductItem = ({items}) => {
                 </div>
               </div>
             </Star>
-              <span className="review_count">리뷰 {item.review_count?item.review_count:0}</span>
+              <span className="review_count">리뷰 {item.REVIEW_COUNT?item.REVIEW_COUNT:0}</span>
             </div>
           </ItemBlock>
-))}
-      </ContentsBlock>
+)}
+      {/* </ContentsBlock> */}
     </>
   )
 }
